3c871230fd510050d87e6e2d6e6aa1154cd2aa62
matheus-amon <matheus.amon@outlook.com>
2026-01-08 08:46:04 -0300

    adicionado brutils e visual da calculadora

--- pages/calculators.py
+++ pages/calculators.py
@@ -1,6 +1,6 @@
 import streamlit as st
+from brutils.currency import format_currency
 
-st.markdown('### Calculadora de Share Rate')
 
 def calculate_share_rate(value, tax, calc):
 	ngr = (value - (value*0.12)) * tax
@@ -11,9 +11,12 @@ def calculate_share_rate(value, tax, calc):
 	else:
 		return ngr
 
+col1, col2 = st.columns(2)
 
-value = st.number_input('Valor', 0.01, icon=':material/money:')
-tax = st.number_input('Taxa', 0.01, 100.00, icon=':material/percent:') / 100
-calc = st.selectbox('Indicador', ['GGR', 'NGR']).lower()
-
-st.metric('Resultado', calculate_share_rate(value, tax, calc))
+c = col1.container(border=True, )
+c.markdown('### Calculadora de Share Rate')
+value = c.number_input('Valor', 0.01, icon=':material/money:')
+tax = c.number_input('Taxa', 0.1, 100.0, 10.0, 0.1,icon=':material/percent:') / 100
+calc = c.selectbox('Indicador', ['GGR', 'NGR']).lower()
+result = format_currency(calculate_share_rate(value, tax, calc))
+c.metric('Resultado', result, border=True)
